47e0bf841b30c753d3da159a7f2a8c88e37939fb
Gabrocecco <gabrocecco@gmail.com>
2023-04-26 14:22:57 +0200

    update codice .png

--- fromXYZtoPNG.py
+++ fromXYZtoPNG.py
@@ -22,11 +22,36 @@ i=0
 for nome_file_xyz in os.listdir(cartellaXYZ):
     if(i>=10):
         break
+    #recupero il path di ogni immagine 
     path_file_xyz = Path.joinpath(cartellaXYZ, nome_file_xyz)
-    print(path_file_xyz)
-    print(nome_file_xyz)
-    Utils.generate_bonds_png(path_file_xyz, cartellaImmaginiPath)
+    #print(path_file_xyz)
+    #print(nome_file_xyz)
+    #chiamo la funzione che tarsforma da .xyz a .png specificando la risoluzione
+    Utils.generate_bonds_png(path_file_xyz, cartellaImmaginiPath, 320)
     i = i+1
+
+
+
+#prova con altri file non di grafene per vedere i colori di più atomi
+# file_xyz = Path('/home/gabro/grapheneDetectProject/data.xyz/subset_xyz/graphene_67.xyz')
+# cartellaImmaginiPath = Path('/home/gabro/grapheneDetectProject/images2')
+# dpath = Path('/home/gabro/grapheneDetectProject')
+# cartellaXYZ = Path('/home/gabro/grapheneDetectProject/xyz_files')
+
+
+# i=0
+# #ciclo delle prime 10 immagini 
+# for nome_file_xyz in os.listdir(cartellaXYZ):
+#     if(i>=10):
+#         break
+#     #recupero il path di ogni immagine 
+#     path_file_xyz = Path.joinpath(cartellaXYZ, nome_file_xyz)
+#     #print(path_file_xyz)
+#     #print(nome_file_xyz)
+#     #chiamo la funzione che tarsforma da .xyz a .png specificando la risoluzione
+#     Utils.generate_bonds_png(path_file_xyz, cartellaImmaginiPath, 320)
+#     i = i+1
+
 
 
 
