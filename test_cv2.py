4b94c6beea0301f011365b960a727cb789c49093
Gabrocecco <gabrocecco@gmail.com>
2023-05-02 17:10:26 +0200

    script di crop aggiunto

--- test_cv2.py
+++ test_cv2.py
@@ -1,17 +1,19 @@
 # import the cv2 library
 import cv2
- 
-# The function cv2.imread() is used to read an image.
-img_grayscale = cv2.imread('/home/gabro/GrapheDetectProject/b74c9ce2-graphene_218641_bonds.png',0)
- 
-# The function cv2.imshow() is used to display an image in a window.
-cv2.imshow('graycsale image',img_grayscale)
+import numpy as np
 
-# waitKey() waits for a key press to close the window and 0 specifies indefinite loop
+img = cv2.imread('/home/gabro/GrapheDetectProject/b74c9ce2-graphene_218641_bonds.png')
+print(img.shape) # Print image shape
+cv2.imshow("original", img)
+ 
+# Cropping an image
+cropped_image = img[18:51, 4:35] #img[start_row:end_row, start_col:end_col]
+
+# Display cropped image
+cv2.imshow("cropped", cropped_image)
+ 
+# Save the cropped image
+cv2.imwrite("Cropped Image.jpg", cropped_image)
+ 
 cv2.waitKey(0)
- 
-# cv2.destroyAllWindows() simply destroys all the windows we created.
 cv2.destroyAllWindows()
- 
-# The function cv2.imwrite() is used to write an image.
-cv2.imwrite('grayscale.jpg',img_grayscale)
--- yolov8_train.py
+++ yolov8_train.py
@@ -4,6 +4,9 @@ from pathlib import Path
 import cv2
 from matplotlib import pyplot as plt
 from matplotlib import image as mpimg
+import torch
+import numpy as np
+import math
 
 #trasformazione .xyz to .png 
 # Utils.from_xyz_to_png(
@@ -28,9 +31,42 @@ results = model("/home/gabro/GrapheDetectProject/b74c9ce2-graphene_218641_bonds.
 
 boxes = results[0].boxes
 
-res_plotted = results[0].plot(labels = False, line_width = 1)
+
+#devo trasformare xy (alto sx), xy (alto dx) in [start_row:end_row, start_col:end_col]
+print(boxes[0].xyxy)    #posizione angolo in alto a sx e in basso a dx in pixel
+array = torch.Tensor.numpy(boxes[0].xyxy)
+print(array.size)
+x1 = math.ceil(array[0,0])
+y1 = math.ceil(array[0,1])
+x2 = math.floor(array[0,2])
+y2 = math.floor(array[0,3])
+print(x1)
+print(y1)
+print(x2)
+print(y2)
+
+# res_plotted = results[0].plot(labels = False, line_width = 1)
 # plt.imshow(res_plotted)
 # plt.show()
-cv2.imshow("result", res_plotted)
+# cv2.imshow("result", res_plotted)
+
+#crop difetto
+img = cv2.imread('/home/gabro/GrapheDetectProject/b74c9ce2-graphene_218641_bonds.png')
+print(img.shape) # Print image shape
+cv2.imshow("original", img)
+ 
+# Cropping an image
+cropped_image = img[y1:y2, x1:x2] #img[start_row:end_row, start_col:end_col]
+
+# Display cropped image
+cv2.imshow("cropped", cropped_image)
+ 
+# Save the cropped image
+cv2.imwrite("Cropped Image.jpg", cropped_image)
+ 
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
 
 # success = model.export(format="onnx")  # export the model to ONNX format
+
