3740a106af088ca43bdd8fbfec373523a128bb7b
Gabrocecco <gabrocecco@gmail.com>
2023-05-02 15:55:39 +0200

    test cv2

--- yolov8_train.py
+++ yolov8_train.py
@@ -1,6 +1,9 @@
 from ultralytics import YOLO
 from lib.lib_utils import Utils 
 from pathlib import Path
+import cv2
+from matplotlib import pyplot as plt
+from matplotlib import image as mpimg
 
 #trasformazione .xyz to .png 
 # Utils.from_xyz_to_png(
@@ -10,16 +13,24 @@ from pathlib import Path
 
 #divisione del dataset in train/ test/
 #inserire percorso dataset da dividere e percentuale del test 
-Utils.split_dataset('/home/gabro/GrapheDetectProject/data_yolo_new/', 0.2)   
+# Utils.split_dataset('/home/gabro/GrapheDetectProject/data_yolo_new/', 0.2)   
 
 # Load a model
 # model = YOLO("yolov8n.yaml")  # build a new model from scratch
-# model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
+model = YOLO("/home/gabro/GrapheDetectProject/best_100_campioni_new.pt")  # load a pretrained model (recommended for training)
 
 # Use the model
 # model.train(data="coco128.yaml", epochs=3)  # train the model
 # model.train(data="config_yolov8.yaml", epochs=100)  # train the model
 
 # metrics = model.val()  # evaluate model performance on the validation set
-# results = model("https://ultralytics.com/images/bus.jpg")  # predict on an image
+results = model("/home/gabro/GrapheDetectProject/b74c9ce2-graphene_218641_bonds.png")  # predict on an image
+
+boxes = results[0].boxes
+
+res_plotted = results[0].plot(labels = False, line_width = 1)
+# plt.imshow(res_plotted)
+# plt.show()
+cv2.imshow("result", res_plotted)
+
 # success = model.export(format="onnx")  # export the model to ONNX format
