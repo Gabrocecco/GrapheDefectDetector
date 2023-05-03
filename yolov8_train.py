de9721ebfbea7ba5c6c396b482cd380388d0127d
Gabrocecco <gabrocecco@gmail.com>
2023-05-03 14:22:45 +0200

    script crop from folder

--- yolov8_train.py
+++ yolov8_train.py
@@ -26,49 +26,51 @@ model = YOLO("/home/gabro/GrapheDetectProject/best_100_campioni_new.pt")  # load
 # model.train(data="coco128.yaml", epochs=3)  # train the model
 # model.train(data="config_yolov8.yaml", epochs=100)  # train the model
 
-# metrics = model.val()  # evaluate model performance on the validation set
-pathImmProva = "/home/gabro/GrapheDetectProject/b74c9ce2-graphene_218641_bonds.png"
-results = model(pathImmProva)  # predict on an image
 
-boxes = results[0].boxes
-index = 0
-print(boxes.data.size)
-# for box in boxes.data:
-#     print(box.xyxy)
-for box in boxes:
-    #devo trasformare xy (alto sx), xy (alto dx) in [start_row:end_row, start_col:end_col]
-    print(boxes[index].xyxy)    #posizione angolo in alto a sx e in basso a dx in pixel
-    array = Torch.Tensor.numpy(boxes[index].xyxy)
-    # print(array.size)
-    x1 = math.ceil(array[0,0])
-    y1 = math.ceil(array[0,1])
-    x2 = math.floor(array[0,2])
-    y2 = math.floor(array[0,3])
+#funzione che fa inferenza su un'immagine e ne salva i crop delle boxes 
+# # metrics = model.val()  # evaluate model performance on the validation set
+# pathImmProva = "/home/gabro/GrapheDetectProject/b74c9ce2-graphene_218641_bonds.png"
+# results = model(pathImmProva)  # predict on an image
 
-    # res_plotted = results[0].plot(labels = False, line_width = 1)
-    # plt.imshow(res_plotted)
-    # plt.show()
-    # cv2.imshow("result", res_plotted)
+# boxes = results[0].boxes
+# index = 0
+# print(boxes.data.size)
+# # for box in boxes.data:
+# #     print(box.xyxy)
+# for box in boxes:
+#     #devo trasformare xy (alto sx), xy (alto dx) in [start_row:end_row, start_col:end_col]
+#     print(boxes[index].xyxy)    #posizione angolo in alto a sx e in basso a dx in pixel
+#     array = Torch.Tensor.numpy(boxes[index].xyxy)
+#     # print(array.size)
+#     x1 = math.ceil(array[0,0])
+#     y1 = math.ceil(array[0,1])
+#     x2 = math.floor(array[0,2])
+#     y2 = math.floor(array[0,3])
 
-    #crop difetto
-    img = cv2.imread(pathImmProva)
-    print(img.shape) # Print image shape
-    # cv2.imshow("original", img)
+#     # res_plotted = results[0].plot(labels = False, line_width = 1)
+#     # plt.imshow(res_plotted)
+#     # plt.show()
+#     # cv2.imshow("result", res_plotted)
+
+#     #crop difetto
+#     img = cv2.imread(pathImmProva)
+#     print(img.shape) # Print image shape
+#     # cv2.imshow("original", img)
     
-    # Cropping an image
-    cropped_image = img[y1:y2, x1:x2] #img[start_row:end_row, start_col:end_col]
+#     # Cropping an image
+#     cropped_image = img[y1:y2, x1:x2] #img[start_row:end_row, start_col:end_col]
 
-    # Display cropped image
-    # cv2.imshow("cropped", cropped_image)
+#     # Display cropped image
+#     # cv2.imshow("cropped", cropped_image)
 
-    nameCropped = pathImmProva.removesuffix('.png') + "_cropped_box_" + str(index)+ ".png"
-    # Save the cropped image
-    cv2.imwrite(nameCropped, cropped_image)
+#     nameCropped = pathImmProva.removesuffix('.png') + "_cropped_box_" + str(index)+ ".png"
+#     # Save the cropped image
+#     cv2.imwrite(nameCropped, cropped_image)
     
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
+#     # cv2.waitKey(0)
+#     # cv2.destroyAllWindows()
 
-    index = index+1
+#     index = index+1
 
 
 # # success = model.export(format="onnx")  # export the model to ONNX format
